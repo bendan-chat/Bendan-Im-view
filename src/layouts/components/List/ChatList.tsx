c54c3a47f4b9f2cadb12da7231c64f7cfa3c2121
王小龙 <1468258057@qq.com>
2023-02-12 15:13:57 +0800

    feat(:bug:): lastMsg 初版提交

--- src/layouts/components/List/ChatList.tsx
+++ src/layouts/components/List/ChatList.tsx
@@ -7,7 +7,7 @@ import { setToAvatar } from "@/redux/modules//chat/action";
 import { List, Avatar, Input, Space, Badge } from "antd";
 import { PlusSquareTwoTone } from "@ant-design/icons";
 
-import { listChat } from "@/api/modules/chat";
+import { clearUnreadChatMsg, getUnreadChatList, listChat } from "@/api/modules/chat";
 import { splitUrlToFileName } from "@/utils/util";
 import { subscribe } from "@/websocket/helper/MyEventEmitter";
 
@@ -15,45 +15,65 @@ import "./ChatList.less";
 
 const ChatList = () => {
 	const navigate = useNavigate();
-	const [data, setData] = useState<Account.ChatUser[]>([]);
+	const [chatUsers, setChatUsers] = useState<Account.ChatUser[]>([]);
 	const { userId } = store.getState().global.userInfo;
+	const [lastMsg, setLastMsg] = useState<Map<number, string>>(new Map<number, string>());
+	// const [lastMsg, setLastMsg] = useState<Map<number, string>>(new Map<number, string>());
 	const [selectId, setSelectId] = useState<number>();
 	const [searchHidden, setSearchHidden] = useState<boolean>(false);
-	const [count, setCount] = useState(0);
+	const [counts, setCounts] = useState<Map<number, number>>(new Map<number, number>());
 
+	/**
+	 * 捕获ws消息 处理成微标
+	 */
 	subscribe("wsMsg", (e: any) => {
-		console.log("ChatList", e);
+		let fromId = Number.parseInt(e.detail.fromId);
+		let toId = Number.parseInt(e.detail.toId);
+		let lastMsg = e.detail.sendContent as string;
+		console.log(toId + ":" + lastMsg);
+		handlerBadgeByfromId(fromId);
+		// handlerLastMsg(lastMsg, toId);
 	});
 
+	/**
+	 * 清空微标
+	 */
+	useEffect(() => {
+		subscribe("clearUnreadMsg", (e: any) => {
+			const { fromId, toId } = e.detail;
+			let fromIdNum = Number.parseInt(fromId);
+			let toIdNum = Number.parseInt(toId);
+			if (!Number.isNaN(fromIdNum) && !Number.isNaN(toIdNum)) {
+				clearUserBadge(fromIdNum, toIdNum);
+			}
+		});
+	}, []);
+
+	/**
+	 * 捕获新增好友消息 然后更新
+	 */
 	useEffect(() => {
 		subscribe("agreeNewFriend", () => {
 			loadChatList();
 		});
 	}, []);
 
+	/**
+	 * 初始加载聊天聊表
+	 */
 	useEffect(() => {
 		loadChatList();
+		loadUnreadChatList();
 	}, []);
-
-	/**
-	 * 加载聊天列表
-	 */
-	function loadChatList() {
-		listChat(userId).then(res => {
-			if (res.success) {
-				setData(res.data);
-			}
-		});
-	}
 
 	/**
 	 * 聊天消息处理
 	 * @param item
 	 */
-	function HandlerLastMsg(item: Account.ChatUser) {
+	function handlerLastMsg(lastMsgArg: string, userId: number) {
 		// * 0文本，1图片，2语音，3视频 , 4文件
 		let msg: string;
-		const msgTemp = splitUrlToFileName(item.lastMsg);
+		const msgTemp = splitUrlToFileName(lastMsgArg);
 		let msgSplit = msgTemp.split(".");
 		if (msgSplit.length == 1) {
 			msg = "str";
@@ -62,19 +82,61 @@ const ChatList = () => {
 		}
 		switch (msg!) {
 			case "str":
-				return msgTemp.length > 10 ? msgTemp.substring(0, 10) + "......" : msgTemp;
+				lastMsg.set(userId, msgTemp.length > 10 ? msgTemp.substring(0, 10) + "......" : msgTemp);
+				break;
 			case "wav":
-				return "[语音]";
+				lastMsg.set(userId, "[语音]");
+				break;
 			case "mp4":
-				return "[视频]";
-			case "jpg":
-				return "[图片]";
-			case "png":
-				return "[图片]";
-			case "gif":
-				return "[图片]";
+				lastMsg.set(userId, "[视频]" + msgTemp);
+				break;
+			case "gif" || "png" || "jpg":
+				lastMsg.set(userId, "[图片]" + msgTemp);
+				break;
 			default:
-				return "[文件]" + msgTemp;
+				lastMsg.set(userId, "[文件]" + msgTemp);
+				break;
+		}
+	}
+
+	/**
+	 * 加载聊天列表
+	 */
+	async function loadChatList() {
+		const { data } = await listChat(userId);
+		setChatUsers(data);
+		data.forEach((v, i) => {
+			handlerLastMsg(v.lastMsg, v.id);
+		});
+		setLastMsg(lastMsg);
+	}
+
+	/**
+	 * 查询未读聊天记录
+	 */
+	async function loadUnreadChatList() {
+		const { data } = await getUnreadChatList(userId);
+		data.map((value, index) => {
+			let fromId = value.fromId;
+			handlerBadgeByfromId(fromId);
+		});
+	}
+
+	/**
+	 * 处理微标数
+	 * @param fromId
+	 */
+	function handlerBadgeByfromId(fromId: number) {
+		if (counts.has(fromId)) {
+			counts.set(fromId, counts.get(fromId)! + 1);
+			setCounts(counts => {
+				return counts;
+			});
+		} else {
+			counts.set(fromId, 1);
+			setCounts(counts => {
+				return counts;
+			});
 		}
 	}
 
@@ -95,6 +157,18 @@ const ChatList = () => {
 		console.log("ClickGroupChat");
 	}
 
+	/**
+	 * 清除用户微标
+	 * @param toId
+	 */
+	function clearUserBadge(userId: number, toId: number) {
+		if (counts.has(toId)) {
+			counts.delete(toId);
+			setCounts(counts);
+			clearUnreadChatMsg(userId, toId);
+		}
+	}
+
 	return (
 		<div>
 			<div className="search-friend-class">
@@ -103,15 +177,16 @@ const ChatList = () => {
 					<PlusSquareTwoTone onClick={ClickGroupChat} style={{ fontSize: "30px" }} />
 				</Space>
 			</div>
-			<div hidden={searchHidden} className="chatList-cha">
+			<div hidden={searchHidden} className="chatList-chat">
 				<List
 					itemLayout="horizontal"
-					dataSource={data}
+					dataSource={chatUsers}
 					renderItem={item => (
 						<List.Item
 							className={`${selectId === item.id ? "active-user" : ""}`}
 							onClick={() => {
 								navigate("/chat" + "/" + item.id);
+								clearUserBadge(userId, item.id);
 								setSelectId(item.id);
 								store.dispatch(setToAvatar(item.avatar as string));
 							}}
@@ -119,12 +194,12 @@ const ChatList = () => {
 							<List.Item.Meta
 								className="index"
 								avatar={
-									<Badge size="small" offset={[2, 2]} count={5}>
+									<Badge size="small" offset={[2, 1]} count={counts.get(item.id)}>
 										<Avatar src={item.avatar} size="large" />
 									</Badge>
 								}
 								title={item.nickName}
-								description={HandlerLastMsg(item)}
+								description={`${lastMsg.get(item.id)}`}
 							/>
 						</List.Item>
 					)}
