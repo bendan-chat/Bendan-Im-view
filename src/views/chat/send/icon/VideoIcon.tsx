74d29ec02309e6d51bbc3bd2bf33c25a8b1b8c54
王小龙 <1468258057@qq.com>
2023-05-08 11:17:56 +0800

    feat: 视频通话

---
+++ src/views/chat/send/icon/VideoIcon.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from "react";
+import { VideoCameraTwoTone } from "@ant-design/icons";
+import { Modal } from "antd";
+import { store } from "@/redux";
+
+interface IProps {
+	toId: number;
+}
+
+export default function PhoneIcon({ toId }: IProps) {
+	const [open, setOpen] = useState<boolean>(false);
+	const { userId } = store.getState().global.userInfo;
+
+	const phoneClick = () => {
+		setOpen(true);
+		const w = window.open("_black");
+		let url = `${import.meta.env.VITE_VIDEO_URL}/webrtc/${userId}.html?toUser=${toId}`;
+		w!.location.href = url;
+	};
+	return (
+		<>
+			<VideoCameraTwoTone onClick={phoneClick} className="video-right-icon" />
+			{/* <Modal
+				closable={false}
+				footer={null}
+				open={open}
+				destroyOnClose={true}
+				centered={true}
+				width={400}
+				onCancel={() => setOpen(false)}
+			>
+				<div className="video-box">
+					<iframe
+						src={`${import.meta.env.VITE_VIDEO_URL}/webrtc/${userId}.html?toUser=${toId}`}
+						frameBorder="0"
+						className="card full-iframe"
+					></iframe>
+				</div>
+			</Modal> */}
+		</>
+	);
+}
